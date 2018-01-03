c14a702a90631788fd02029146bd23e61aabe98f
shiyongbin <shiyongbin@ezbuy.com>
2018-01-03 10:58:39 +0800

    test Seach func: join

--- example/model/model_suite_test.go
+++ example/model/model_suite_test.go
@@ -1,13 +1,35 @@
 package model_test
 
 import (
+	"testing"
+
+	. "github.com/ezbuy/redis-orm/example/model"
+
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
-
-	"testing"
 )
+
+func init() {
+	MySQLSetup(&MySQLConfig{
+		Host:     "localhost",
+		Port:     3306,
+		UserName: "ezorm_user",
+		Password: "ezorm_pass",
+		Database: "ezorm",
+	})
+
+	MySQL().Debug(true)
+}
 
 func TestModel(t *testing.T) {
 	RegisterFailHandler(Fail)
 	RunSpecs(t, "redis-orm")
+}
+
+/*
+SELECT blogs.`id`,blogs.`user_id`,blogs.`title`,blogs.`content`,blogs.`status`,blogs.`readed`,blogs.`created_at`,blogs.`updated_at` FROM blogs INNER JOIN users ON blog.user_id = users.user_id WHERE users.mail_box=?   [test@ezbuy.com]
+*/
+func TestSearch(t *testing.T) {
+	blogs, err := BlogDBMgr(MySQL()).Search("INNER JOIN users ON blog.user_id = users.user_id WHERE users.mail_box=?", "", "", "test@ezbuy.com")
+	_, _ = blogs, err
 }
