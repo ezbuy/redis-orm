bc96c2a517095a8a6b9eb4891999b177764af1b5
scnace <scbizu@gmail.com>
2018-12-07 20:13:10 +0800

    trace: impl execContext

--- trace/database/mysql/mysql.go
+++ trace/database/mysql/mysql.go
@@ -56,7 +56,19 @@ func (t *DefaultTracer) QueryContext(ctx context.Context, sql string, args ...in
 }
 
 func (t *DefaultTracer) ExecContext(ctx context.Context, sql string, args ...interface{}) (sql.Result, error) {
-	return t.db.ExecContext(ctx, sql, args...)
+	if span := opentracing.SpanFromContext(ctx); span != nil {
+		span := t.tracer.StartSpan(dbTracerServiceMySQL, opentracing.ChildOf(span.Context()))
+		tags.SpanKindRPCClient.Set(span)
+		tags.PeerService.Set(span, dbTracerServiceMySQL)
+		span.SetTag("sql.query", t.hackQueryBuilder(sql, args...))
+		ctx = opentracing.ContextWithSpan(ctx, span)
+		defer span.Finish()
+	}
+	res, err := t.db.ExecContext(ctx, sql, args...)
+	if err != nil {
+		return nil, err
+	}
+	return res, nil
 }
 
 func (t *DefaultTracer) hackQueryBuilder(query string, args ...interface{}) string {
